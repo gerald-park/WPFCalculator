6f1d50ac6646cd7da80558ce6503db0b0fc77837
Gerald Park <gerald.park@edwardsvacuum.com>
2023-01-07 10:27:17 +0900

    explicit style

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -92,7 +92,7 @@ namespace Calculator
                 tempNumber = lastNumber / 100;
                 if (lastNumber != 0)
                     tempNumber *= lastNumber;
-                resultLabel.Content = lastNumber.ToString();
+                resultLabel.Content = tempNumber.ToString();
             }
         }
 
